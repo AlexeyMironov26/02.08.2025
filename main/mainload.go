fd877e1f5f9acdd41eab69d94489bac03a40f4ab
AlexeyMironov26 <maa937866@gmail.com>
2025-08-02 00:51:33 +0300

    firstcom

---
+++ main/mainload.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Linkfj struct {
+	URL string `json:"url"`
+}
+type Status string
+
+const (
+	StatusInitialized Status = "initialized"
+	StatusInProgress         = "in progress"
+	StatusReady              = "Ready"
+)
+
+type Archive struct {
+	Arch     bytes.Buffer
+	Objs     int
+	zpwrr    *zip.Writer
+	ArchStat Status
+	Link     string
+}
+
+var n int
+var idcount int
+var archs map[int]*Archive = make(map[int]*Archive, 8)
+
+//for i:=0; i<3; i++{
+//archs[i].Arch=bytes.newBuffer([]byte{})
+//}
+
+func main() {
+	http.HandleFunc("/ini_arch", initArchive)
+	http.HandleFunc("/add_obj", addObject)
+	http.HandleFunc("/arch_stat", getArchStatus)
+	http.HandleFunc("/archive/", downloadArch)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func initArchive(w http.ResponseWriter, r *http.Request) {
+	if n == 3 {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusConflict)
+		w.Write([]byte("You already have 3 not formed archives, you can't have more ones at once. Server is busy."))
+		return
+	}
+	idcount += 1
+	n += 1
+	archs[idcount] = &Archive{}
+	buf := archs[idcount].Arch
+	archs[idcount].zpwrr = zip.NewWriter(&buf)
+	archs[idcount].ArchStat = StatusInitialized
+	w.Write([]byte("The id of created task by formig a zip archive is: " + strconv.Itoa(idcount)))
+	r.Body.Close()
+}
+
+func addObject(w http.ResponseWriter, r *http.Request) {
+
+	defer r.Body.Close()
+	if r.Method != "PATCH" {
+		http.Error(w, "You should do a PATCH request", http.StatusMethodNotAllowed)
+		return
+	}
+	id, err := helpy1(w, r)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if archs[id].Objs == 3 {
+		http.Error(w, "Archive with this id already contains 3 objects and its task was finished",
+			http.StatusConflict)
+		return
+	}
+	tmpl := strings.Split(r.URL.String(), "/")
+	archlink := strings.Join([]string{tmpl[0], tmpl[1], tmpl[2]}, "/") + "/archive/" + strconv.Itoa(id)
+	link := Linkfj{}
+	byts, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if err := json.Unmarshal(byts, &link); err != nil {
+		log.Print(err)
+		http.Error(w, "Please create the body of the request in json format properly", http.StatusBadRequest)
+		return
+	}
+	resp, err := http.Get(link.URL)
+	// if _,ok:=err.(*url.Error); ok{
+	//     log.Print(err)
+	// 	http.Error(w, "Your URL from patch request is in a wrong format", http.StatusBadRequest)
+	// 	return
+	// }else
+	if err != nil {
+		log.Print(err)
+		http.Error(w, `Perhaps, your URL from patch request is in a wrong
+         format or server where the resource located, is not available`, http.StatusBadRequest)
+		return
+	}
+	defer resp.Body.Close()
+	if !(strings.HasSuffix(link.URL, ".jpeg") || strings.HasSuffix(link.URL, ".pdf")) {
+		http.Error(w, "Supported for downloading only files of formats \".jpeg\" and \".pdf\"", http.StatusConflict)
+		return
+	}
+	if !(resp.StatusCode == http.StatusNonAuthoritativeInfo || resp.StatusCode == http.StatusOK) {
+		http.Error(w, "The resource is unavailable for downloading, but now at this archive left less space",
+			http.StatusNotFound)
+		archs[id].Objs += 1
+		if archs[id].Objs == 3 {
+			archs[id].ArchStat = StatusReady
+			archs[id].Link = archlink
+			n -= 1
+			if archs[id].Arch.Len() == 0 {
+				http.Error(w, `All 3 objects of archive are not available for download, this archive 
+                is empty and will be deleted`,
+					http.StatusCreated)
+				delete(archs, id)
+			}
+			archs[id].zpwrr.Close()
+		} else {
+			archs[id].ArchStat = StatusInProgress
+		}
+		return
+	}
+	urlparts := strings.Split(link.URL, "/")
+	filewrr, err := archs[id].zpwrr.Create(urlparts[len(urlparts)-1])
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	_, err1 := filewrr.Write(byts)
+	if err1 != nil {
+		log.Print(err1)
+		return
+	}
+	archs[id].Objs += 1
+	if archs[id].Objs == 3 {
+		n -= 1
+		archs[id].ArchStat = StatusReady
+		archs[id].Link = archlink
+		archs[id].zpwrr.Close()
+	} else {
+		archs[id].ArchStat = StatusInProgress
+	}
+}
+
+func getArchStatus(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	id, err := helpy1(w, r)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("The status of this task with id: %d by creating zip archive is %s",
+		id, archs[id].ArchStat)))
+	if archs[id].ArchStat == StatusReady {
+
+	}
+
+}
+
+func helpy1(w http.ResponseWriter, r *http.Request) (int, error) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "You made a mistake in transfering parametres", http.StatusBadRequest)
+		return 0, errors.New("parameters are in a wrong format")
+	}
+	if amt := r.Form.Get("id"); amt != "" {
+		idt, err := strconv.Atoi(amt)
+		if err != nil {
+			http.Error(w, "You made a mistake in transfering parametres", http.StatusBadRequest)
+			return 0, errors.New("parameters are in a wrong format")
+		}
+		_, ok := archs[idt]
+		if !ok {
+			http.Error(w, "There are no task with such id", http.StatusNotFound)
+			return 0, errors.New("not existing task id")
+		}
+		return idt, nil
+	} else {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`You didn't specify the id of archive, which status you need to know, in parameters 
+        of your request`))
+		return 0, errors.New("parameters not found")
+	}
+}
+
+func downloadArch(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(strings.Split("http://"+r.Host+r.URL.Path, "/")[4])
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	btdata, err1 := io.ReadAll(&archs[id].Arch)
+	if err1 != nil {
+		log.Print(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/zip")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"archive%d.zip\"", id))
+	w.Header().Set("Content-Length", strconv.Itoa(len(btdata)))
+	w.Write(btdata)
+
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+My server use http as main protocol for interaction with it and the server is launching on 0.0.0.0 (means that my server after launching accepts and processes http requests on all ip-addresses which this machine possesses) (IP):8080(port) it is for shaping zip archives from files found on internet and providing a link for downoading that archive. the example of url to address to host: http://localhost:8080/
+Launch it as go run mainload.go in one terminal and send http requests on api of server from another(you can utility "curl" for that). To shut down the server push ctrl+c into the terminal where it was ran.
+
+Server have the following endpoints:
+
+1). /ini_arch
+you should send post request on this endpoint to initialize new task by creating an archive and as a response you'll obtain the id of this new task. Not more than 3 tasks can be in progress simultaneously (but nevertheless you able to get access to unlimited amount of ready archives by id of its tasks sending requests at endpoint /arch_stat)
+example: curl -X POST "http://localhost:8080/ini_arch"
+
+2). /add_obj
+    this endpoint is for adding objects in particular archive, which we already began to form in our task. You should specify as parameter in url of your request the id of task, to which archive you wish to add your object (only .jpeg and .pdf formats are permited). One archive should contain 3 files. The body of patch request should have header Content-type : application/json and contain a url from the internet on resourse like {
+        "url": "https://site.com/imageee3576888.jpeg"
+    }
+    example: curl -X PATCH -H "Content-Type: application/json" -d '{"url":"https://somesite.com/doc67.pdf"}' "http://localhost:8080/add_obj?id=3"
+
+3). /arch_stat
+you need to send on this endpoint get request with parameter "id" in url of request, which will point on a particular task (remmeber the ids of all your tasks when you create it sending request on endpoint /ini_arch) and in response you will get the status of task: "initialized"-archive without any objects, "in progress" - archive have 1-2 objects - or "ready" - archive have 3 loaded objects, in this situation with status you get as well a link for downloading this archive.
+example: curl "http://localhost:8080/arch_stat?id=3"
+4) /archive/{id of ended task} - it is a group of endpoints including links on all formed archives, managed by one handler with endpoint /archive/. When after checking status of archive you find it "ready", you get the special archive link on this server, to get the archive-zip file in body of response you should send a get-request on that special link. 
+example: You can download it on your computer with different ways, like with help of curl which will download the obtained data from get request on specified url: curl -L -O http://localhost:8080/archive/3
